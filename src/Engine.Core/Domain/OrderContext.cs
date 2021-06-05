55db5e342300b68cdf4939dd93268ab6eecbb829
nkiok <nikoskiok@gmail.com>
2021-06-05 12:01:42 +0100

    add context class for rules

---
+++ src/Engine.Core/Domain/OrderContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Engine.Core.Domain
+{
+    public class OrderContext
+    {
+        public OrderMaster OrderMaster { get; }
+
+        public Product Product { get; }
+        
+        public OrderContext(OrderMaster orderMaster, Product product)
+        {
+            OrderMaster = orderMaster;
+
+            Product = product;
+        }
+    }
+}
