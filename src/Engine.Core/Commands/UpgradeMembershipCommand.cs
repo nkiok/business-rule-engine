d81d95afb70418c11b665a7cbacd04206c164c5f
nkiok <nikoskiok@gmail.com>
2021-06-05 12:16:34 +0100

    add command for upgrade membership rule as per problem statement rule #4

---
+++ src/Engine.Core/Commands/UpgradeMembershipCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Engine.Core.Domain;
+
+namespace Engine.Core.Commands
+{
+    public class UpgradeMembershipCommand : BaseCommand
+    {
+        public UpgradeMembershipCommand(Product product, Guid orderId) : base(product, orderId)
+        {
+        }
+    }
+}
