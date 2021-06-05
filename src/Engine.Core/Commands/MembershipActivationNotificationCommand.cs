0f6e3b1c0c8e3435dc9611742d0268a3410e252a
nkiok <nikoskiok@gmail.com>
2021-06-05 12:18:30 +0100

    add commands for membership notification rules as per problem statement rule #5

---
+++ src/Engine.Core/Commands/MembershipActivationNotificationCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Engine.Core.Domain;
+
+namespace Engine.Core.Commands
+{
+    public class MembershipActivationNotificationCommand : BaseCommand
+    {
+        public MembershipActivationNotificationCommand(Product product, Guid orderId) : base(product, orderId)
+        {
+        }
+    }
+}
---
+++ src/Engine.Core/Commands/MembershipUpgradeNotificationCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Engine.Core.Domain;
+
+namespace Engine.Core.Commands
+{
+    public class MembershipUpgradeNotificationCommand : BaseCommand
+    {
+        public MembershipUpgradeNotificationCommand(Product product, Guid orderId) : base(product, orderId)
+        {
+        }
+    }
+}
