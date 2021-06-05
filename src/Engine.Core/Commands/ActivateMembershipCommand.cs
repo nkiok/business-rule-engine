2b2df9dcec7055329e0d6e847772dd880c29c8a0
nkiok <nikoskiok@gmail.com>
2021-06-05 12:15:54 +0100

    add command for new membership product rule as per problem statement rule #3

---
+++ src/Engine.Core/Commands/ActivateMembershipCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Core.Domain;
+
+namespace Engine.Core.Commands
+{
+    public class ActivateMembershipCommand : BaseCommand
+    {
+        public ActivateMembershipCommand(Product product, Guid orderId) : base(product, orderId)
+        {
+        }
+    }
+}
