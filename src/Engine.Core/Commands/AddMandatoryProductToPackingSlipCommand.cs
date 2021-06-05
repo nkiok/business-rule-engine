64fd0b56db9f469088ad98109865e264395bf890
nkiok <nikoskiok@gmail.com>
2021-06-05 12:20:42 +0100

    add command for mandatory product to be included in packing list as per problem statement rule #6

---
+++ src/Engine.Core/Commands/AddMandatoryProductToPackingSlipCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Engine.Core.Domain;
+
+namespace Engine.Core.Commands
+{
+    public class AddMandatoryProductToPackingSlipCommand : BaseCommand
+    {
+        public AddMandatoryProductToPackingSlipCommand(Product product, Guid orderId) : base(product, orderId)
+        {
+        }
+    }
+}
