ea6ad18f7c8079cd7547f8637239252ae3272976
nkiok <nikoskiok@gmail.com>
2021-06-05 12:27:32 +0100

    add packing slip rule handler as per problem statement rule #1

---
+++ src/Engine.Core/Domain/Consts.cs
@@ -0,0 +1,7 @@
+﻿namespace Engine.Core.Domain
+{
+    public class Consts
+    {
+        public const string RuleDoesNotApply = "Rule does not apply";
+    }
+}
---
+++ src/Engine.Core/Rules/PackingSlipRule.cs
@@ -0,0 +1,25 @@
+﻿using CSharpFunctionalExtensions;
+using Engine.Core.Commands;
+using Engine.Core.Domain;
+
+namespace Engine.Core.Rules
+{
+    public class PackingSlipRule : IBusinessRule<OrderContext>
+    {
+        private readonly ISender _sender;
+
+        public PackingSlipRule(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        public Result Apply(OrderContext context) 
+            => Result.SuccessIf(Predicate(context.Product), Consts.RuleDoesNotApply).Tap(() => SendCommand(context));
+
+        private static bool Predicate(Product product) 
+            => product.Physical;
+
+        private void SendCommand(OrderContext context) 
+            => _sender.Send(new AddProductToPackingSlipCommand(context.Product, context.OrderMaster.Id));
+    }
+}
