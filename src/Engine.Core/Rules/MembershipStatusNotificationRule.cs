644dff61944297b0df860cca3a5659faf4a3208b
nkiok <nikoskiok@gmail.com>
2021-06-05 12:38:34 +0100

    add membership notifications rule handler and tests as per problem statement rule #5

---
+++ src/Engine.Core/Rules/MembershipStatusNotificationRule.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using CSharpFunctionalExtensions;
+using Engine.Core.Commands;
+using Engine.Core.Domain;
+
+namespace Engine.Core.Rules
+{
+    public class MembershipStatusNotificationRule : IBusinessRule<OrderContext>
+    {
+        private readonly ISender _sender;
+
+        private readonly IDictionary<ProductType, Func<OrderContext, Result>> _notificationMap =  new Dictionary<ProductType, Func<OrderContext, Result>>();
+
+        public MembershipStatusNotificationRule(ISender sender)
+        {
+            _sender = sender;
+
+            _notificationMap[ProductType.NewMembership] = SendActivationNotificationCommand;
+            _notificationMap[ProductType.UpgradeMembership] = SendUpgradeNotificationCommand;
+        }
+
+        public Result Apply(OrderContext context) 
+            => Result.SuccessIf(Predicate(context.Product), Consts.RuleDoesNotApply).Tap(() => _notificationMap[context.Product.Type].Invoke(context));
+
+        private static bool Predicate(Product product) 
+            => product.Type == ProductType.NewMembership || product.Type == ProductType.UpgradeMembership;
+
+        private Result SendActivationNotificationCommand(OrderContext context)
+        {
+            _sender.Send(new MembershipActivationNotificationCommand(context.Product, context.OrderMaster.Id));
+
+            return Result.Success();
+        }
+
+        private Result SendUpgradeNotificationCommand(OrderContext context)
+        {
+            _sender.Send(new MembershipUpgradeNotificationCommand(context.Product, context.OrderMaster.Id));
+
+            return Result.Success();
+        }
+    }
+}
---
+++ src/Engine.Tests/Rules/MembershipStatusNotificationRuleTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using CSharpFunctionalExtensions;
+using Engine.Core.Commands;
+using Engine.Core.Domain;
+using Engine.Core.Rules;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Engine.Tests.Rules
+{
+    public class MembershipStatusNotificationRuleTests
+    {
+        private IBusinessRule<OrderContext> _rule;
+        private ISender _sender;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sender = Substitute.For<ISender>();
+
+            _rule = new MembershipStatusNotificationRule(_sender);
+        }
+        
+        [Test]
+        [TestCase(ProductType.NewMembership, ExpectedResult = true, TestName = "Notification for a new membership")]
+        [TestCase(ProductType.UpgradeMembership, ExpectedResult = true, TestName = "Notification for a membership upgrade")]
+        [TestCase(ProductType.Book, ExpectedResult = false, TestName = "No notification for a book")]
+        public bool RuleEvaluationReturnsExpectedResultFor(ProductType productType)
+        {
+            var orderContext = CreateOrderContextWith(productType);
+
+            var result = _rule.Apply(orderContext)
+                .TapIf(productType == ProductType.NewMembership, () => 
+                    _sender.Received().Send(Arg.Any<MembershipActivationNotificationCommand>()))
+                .TapIf(productType == ProductType.UpgradeMembership, () => 
+                    _sender.Received().Send(Arg.Any<MembershipUpgradeNotificationCommand>()))
+                .OnFailure(() =>
+                    {
+                        _sender.DidNotReceive().Send(Arg.Any<MembershipActivationNotificationCommand>());
+                        _sender.DidNotReceive().Send(Arg.Any<MembershipUpgradeNotificationCommand>());
+                    });
+ 
+            return result.IsSuccess;
+        }
+
+        private static OrderContext CreateOrderContextWith(ProductType productType)
+            => new OrderContext(
+                new OrderMaster(),
+                new Product(Guid.NewGuid(), false, productType, string.Empty));
+    }
+}
