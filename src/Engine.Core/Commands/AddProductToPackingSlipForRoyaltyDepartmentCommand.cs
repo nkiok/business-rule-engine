544e8fef0272b274cb774b95dda116238a30bcc7
nkiok <nikoskiok@gmail.com>
2021-06-05 12:15:02 +0100

    add command for book product rule as per problem statement rule #2

---
+++ src/Engine.Core/Commands/AddProductToPackingSlipForRoyaltyDepartmentCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Engine.Core.Domain;
+
+namespace Engine.Core.Commands
+{
+    public class AddProductToRoyaltyDepartmentPackingSlipCommand : BaseCommand
+    {
+        public AddProductToRoyaltyDepartmentPackingSlipCommand(Product product, Guid orderId) : base(product, orderId)
+        {
+        }
+    }
+}
