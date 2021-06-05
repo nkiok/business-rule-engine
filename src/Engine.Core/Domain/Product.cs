b74b8aae280be18a5237491d9cee7a189dc19fe3
nkiok <nikoskiok@gmail.com>
2021-06-05 11:56:21 +0100

    add product class

---
+++ src/Engine.Core/Domain/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Engine.Core.Domain
+{
+    public class Product
+    {
+        public Guid Id { get; }
+
+        public bool Physical { get; }
+
+        public ProductType Type { get; }
+
+        public string Description { get; }
+
+        public Product(Guid id, bool physical, ProductType type, string description)
+        {
+            Id = id;
+
+            Physical = physical;
+
+            Type = type;
+
+            Description = description;
+        }
+    }
+}
