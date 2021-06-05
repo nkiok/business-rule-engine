b060df482c17fad741c37b61f287efab53b5afc5
nkiok <nikoskiok@gmail.com>
2021-06-05 12:22:11 +0100

    add command for agent commission as per problem statement rule #7

---
+++ src/Engine.Core/Commands/CreateAgentCommissionCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Engine.Core.Domain;
+
+namespace Engine.Core.Commands
+{
+    public class CreateAgentCommissionCommand : BaseCommand
+    {
+        public Guid AgentId { get; }
+
+        public CreateAgentCommissionCommand(Product product, Guid orderId, Guid agentId) : base(product, orderId)
+        {
+            AgentId = agentId;
+        }
+    }
+}
