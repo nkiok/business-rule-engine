8cbd5f1da560c2ae9333597a774bd4e5ace01797
nkiok <nikoskiok@gmail.com>
2021-06-05 12:03:51 +0100

    composite for processing rules

---
+++ src/Engine.Core/RulesProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Engine.Core.Domain;
+using Engine.Core.Rules;
+
+namespace Engine.Core
+{
+    public class RulesProcessor
+    {
+        private readonly IEnumerable<IBusinessRule<OrderContext>> _rules;
+
+        public RulesProcessor(IEnumerable<IBusinessRule<OrderContext>> rules)
+        {
+            _rules = rules;
+        }
+
+        public void ApplyRules(OrderContext context)
+        {
+            foreach (var rule in _rules)
+            {
+                rule.Apply(context);
+            }
+        }
+    }
+}
