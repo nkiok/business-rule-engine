2cbe8e7998532058fab90f25fcc581c522cd0de3
nkiok <nikoskiok@gmail.com>
2021-06-05 12:13:10 +0100

    add command for physical product rule as per problem statement rule #1

---
+++ src/Engine.Core/Commands/AddProductToPackingSlipCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Engine.Core.Domain;
+
+namespace Engine.Core.Commands
+{
+    public class AddProductToPackingSlipCommand : BaseCommand
+    {
+        public AddProductToPackingSlipCommand(Product product, Guid orderId) : base(product, orderId)
+        {
+        }
+    }
+}
