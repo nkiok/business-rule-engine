a8a1232e575190ec040a1ae174e46241bf8129b9
nkiok <nikoskiok@gmail.com>
2021-06-05 12:07:04 +0100

    business rule engine, order to ordercontext

---
+++ src/Engine.Core/OrderRulesEngine.cs
@@ -0,0 +1,22 @@
+﻿using Engine.Core.Domain;
+
+namespace Engine.Core
+{
+    public class OrderRulesEngine
+    {
+        private readonly RulesProcessor _rulesProcessor;
+
+        public OrderRulesEngine(RulesProcessor rulesProcessor)
+        {
+            _rulesProcessor = rulesProcessor;
+        }
+
+        public void Run(Order order)
+        {
+            foreach (var product in order.Detail.Products)
+            {
+                _rulesProcessor.ApplyRules(new OrderContext(order.Master, product));
+            }
+        }
+    }
+}
